8798ec03ed012524f5c889f218b885a76e02c677
marlisapee <mpee@tripadvisor.com>
2024-05-02 18:42:26 -0400

    created model associations in index

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,9 +1,17 @@
 const User = require('./User');
 const Deck = require('./Deck');
-
-// import the rest of your models above
+const Card = require('./Card');
+const Attack = require('./Attack');
 
 // set up the associations here
+User.hasOne(Deck);
+Deck.belongsTo(User);
+
+Deck.hasMany(Card);
+Card.belongsTo(Deck);
+
+Card.belongsToMany(Attack, { through: 'CardAttacks' });
+Attack.belongsToMany(Card, { through: 'CardAttacks' });
 
 // and then export them all below
 module.exports = { User };
